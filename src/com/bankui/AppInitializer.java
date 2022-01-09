e6d23dffadfd6d826fe9f26fd40684957d2a5272
unknown <shivalishinde7@gmail.com>
2022-01-09 16:43:16 +0530

    mvc configuration

---
+++ src/com/bankui/AppInitializer.java
@@ -0,0 +1,25 @@
+package com.bankui;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[]{"/"};
+	}
+
+}
